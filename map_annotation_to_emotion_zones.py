3a68d7448061cbf7be729e1603cf4979e9ef1c60
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-06-23 16:04:46 +0100

    mapping valence, arousal into the 4 emotion zones.

---
+++ map_annotation_to_emotion_zones.py
@@ -0,0 +1,41 @@
+"""
+Script to map values of arousal and valence into the 4 emotion zones.
+"""
+import numpy as np
+
+emotion_zone = {
+    'blue': np.array([1, 0, 0, 0]),
+    'green': np.array([0, 1, 0, 0]),
+    'yellow': np.array([0, 0, 1, 0]),
+    'red': np.array([0, 0, 0, 1])
+}
+
+
+def get_emotion_zone(arousal, valence):
+    """
+    each emotion zone is represented by an np array of 4 dimension
+    """
+    emotion = None
+    if arousal == 0 and valence == 0:
+        emotion = 'green'
+    elif arousal > 0:
+        if valence >= 0:
+            emotion = 'yellow'
+        else:
+            emotion = 'red'
+    elif arousal < 0:
+        if valence > 0:
+            emotion = 'green'
+        else:
+            emotion = 'blue'
+    elif arousal == 0:
+        if valence > 0:
+            emotion = 'green'
+        else:
+            emotion = 'red'
+    elif valence == 0:
+        if arousal > 0:
+            emotion = 'yellow'
+        else:
+            emotion = 'blue'
+    return emotion
