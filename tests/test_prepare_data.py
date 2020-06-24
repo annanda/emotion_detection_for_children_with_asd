37e0572f9912645b7dd4414e1ad71ffff9edff2c
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-06-24 11:53:27 +0100

    organising project structure.

--- tests/test_prepare_data.py
+++ tests/test_prepare_data.py
@@ -1,5 +1,5 @@
 import unittest
-from map_annotation_to_emotion_zones import get_emotion_zone
+from prepare_data.map_annotation_to_emotion_zones import get_emotion_zone
 
 
 class MapAnnotationTest(unittest.TestCase):
@@ -27,3 +27,23 @@ class MapAnnotationTest(unittest.TestCase):
         emotion = get_emotion_zone(-1, 0)
         correct = 'red'
         self.assertEqual(emotion, correct)
+
+    def test_mapping_emotion_zones_green(self):
+        emotion = get_emotion_zone(0.5, -0.5)
+        self.assertEqual(emotion, 'green')
+
+    def test_mapping_emotion_zones_yellow(self):
+        emotion = get_emotion_zone(0.5, 0.5)
+        self.assertEqual(emotion, 'yellow')
+
+    def test_mapping_emotion_zones_blue(self):
+        emotion = get_emotion_zone(-0.5, -0.5)
+        self.assertEqual(emotion, 'blue')
+
+    def test_mapping_emotion_zones_red(self):
+        emotion = get_emotion_zone(-0.5, 0.5)
+        self.assertEqual(emotion, 'red')
+
+    def test_mapping_emotion_zones_random(self):
+        emotion = get_emotion_zone(0.238405, 0.3618)
+        self.assertEqual(emotion, 'yellow')
