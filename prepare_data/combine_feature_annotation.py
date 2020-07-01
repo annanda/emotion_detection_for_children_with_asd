0da6d0e0656e337dc05385b1a32b7e3b5e9ede5e
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-01 07:57:46 +0100

    Finishing the combining features + annotation script.

--- prepare_data/combine_feature_annotation.py
+++ prepare_data/combine_feature_annotation.py
@@ -1,44 +1,47 @@
-import glob
+"""
+author: Annanda Sousa
+script to generate a CSV with the combined values of features and annotation for each 'frametime' of the dataset.
+"""
 import pathlib
 import os.path
 import pandas as pd
 from scipy.io import arff
 import glob
 
-path_main = pathlib.Path(__file__).parent.parent.absolute()
-path_data_train = os.path.join(path_main, 'features', 'video', 'AU')
-# files = glob.glob(f"{path_data_train}/*.arff")
-path_absolute = '/Users/user/PycharmProjects/emotion_detection_system/features/video/AU/train_1_3.0_0.4.arff'
-data_train = arff.loadarff(path_absolute)
-data_train = pd.DataFrame(data_train[0])
-
-data_emotion_annotation = pd.read_csv(
-    '/Users/user/PycharmProjects/emotion_detection_system/labels/emotion_zones/emotion_names/train_1.csv')
-
-result = pd.merge(data_train, data_emotion_annotation, how='inner', on='frametime')
-result.dropna(inplace=True)
-df_new = result.drop_duplicates()
-
-print(result)
+path_main_folder = pathlib.Path(__file__).parent.parent.absolute()
 
 
-# df_new.to_csv(f"{path_arousal}/{file_name}.csv", index=False)
-
-
-def merge_features_annotation(file_features, file_names_emotions):
+def merge_features_with_annotation(file_features, list_file_name_emotions):
     file_name_suffix = file_features.split('/')[-1]
-    file_name_suffix = file_name_suffix.split('_3')[0]
-    data_features = arff.loadarff(path_absolute)
-    data_features = pd.DataFrame(data_features[0])
-    file_emotions = [file for file in file_names_emotions if file_name_suffix in file][0]
-    data_emotion_annotation = pd.read_csv(file_emotions)
-    result = pd.merge(data_features, data_emotion_annotation, how='inner', on='frametime')
-    result.dropna(inplace=True)
-    print(result)
-    path_dataset = ''
-    df_new.to_csv(f"{path_dataset}/{file_name_suffix}.csv", index=False)
+    file_name_suffix = file_name_suffix.split('_3.0_0')[0]
+    data_features = arff.loadarff(file_features)
+    data_features_df = pd.DataFrame(data_features[0])
+    file_emotions = [file for file in list_file_name_emotions if file_name_suffix in file][0]
+    data_emotion_annotation_df = pd.read_csv(file_emotions)
+    merged = pd.merge(data_features_df, data_emotion_annotation_df, how='inner', on='frametime')
+    merged.dropna(inplace=True)
+    path_dataset = os.path.join(path_main_folder, 'dataset', 'video', 'AU')
+    merged.to_csv(f"{path_dataset}/{file_name_suffix}.csv", index=False)
+
+
+def merge_for_all_files(list_file_features, list_file_annotation):
+    for list_train in list_file_features:
+        merge_features_with_annotation(list_train, list_file_annotation)
+
+
+def call_merge_video_au(list_file_annotation):
+    """
+    calling the function merge_for_all_files() for audio modality - features Action Units (AU)
+    """
+    path_features_train = os.path.join(path_main_folder, 'features', 'video', 'AU')
+    list_file_features_train = glob.glob(f"{path_features_train}/*.arff")
+    # eliminating the test files from the list, because we don't have annotation for test files.
+    list_file_features_train = [file for file in list_file_features_train if 'test' not in file]
+    list_file_features_train.sort()
+    merge_for_all_files(list_file_features_train, list_file_annotation)
+
 
 if __name__ == '__main__':
-    file_features = '/Users/user/PycharmProjects/emotion_detection_system/features/video/AU/train_1_3.0_0.4.arff'
-    file_name_emotions = glob.glob("/Users/user/PycharmProjects/emotion_detection_system/labels/emotion_zones/emotion_names/*.csv")
-    merge_features_annotation(file_features, file_name_emotions)
+    path_annotation_emotions = os.path.join(path_main_folder, 'labels', 'emotion_zones', 'emotion_names')
+    list_file_annotation_emotions = glob.glob(f"{path_annotation_emotions}/*.csv")
+    call_merge_video_au(list_file_annotation_emotions)
