3d838236456e5e104367a97f0c5f53d04377d0f4
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-01 09:26:37 +0100

    including config file.

---
+++ setup/conf.py
@@ -0,0 +1,12 @@
+import pathlib
+import os.path
+
+from decouple import config
+
+main_folder = pathlib.Path(__file__).parent.parent.absolute()
+
+MAIN_FOLDER = config('MAIN_FOLDER', default=main_folder)
+emotion_folder = os.path.join(MAIN_FOLDER, 'labels', 'emotion_zones', 'emotion_names')
+
+EMOTION_ANNOTATION_FOLDER = config('EMOTION_ANNOTATION_FOLDER', default=emotion_folder)
+DATASET_VIDEO_AU_FOLDER = config('DATASET_VIDEO_AU', default=os.path.join(MAIN_FOLDER, 'dataset/video/AU'))
