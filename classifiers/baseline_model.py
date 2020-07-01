d3813f1a1671420a524867e0bceb04b4a230158a
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-01 12:18:19 +0100

    Finishing creating and accuracy of a dummy baseline model - for ED using AU from video modality.

--- classifiers/baseline_model.py
+++ classifiers/baseline_model.py
@@ -1,11 +1,20 @@
 from sklearn.dummy import DummyClassifier
 import pandas as pd
 from setup.conf import DATASET_VIDEO_FOLDER
+from sklearn.metrics import accuracy_score
+
+from sklearn.model_selection import train_test_split
 
 x = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv').iloc[:, 2:-1]
 y = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv')['emotion_zone']
-# dummy_clf = DummyClassifier(strategy="most_frequent")
-dummy_clf = DummyClassifier(strategy="uniform")
+
+x_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_dev.csv').iloc[:, 2:-1]
+y_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_dev.csv')['emotion_zone']
+x_dev, x_test = train_test_split(x_dev_dataset, test_size=0.2)
+y_dev, y_test = train_test_split(y_dev_dataset, test_size=0.2)
+dummy_clf = DummyClassifier(strategy="most_frequent")
+# dummy_clf = DummyClassifier(strategy="uniform")
 dummy_clf.fit(x, y)
-predictions = dummy_clf.predict(x[:10])
-print(predictions)
+predictions = dummy_clf.predict(x_test)
+accuracy = accuracy_score(y_test, predictions)
+print(f'Accuracy of baseline model: {accuracy}')
