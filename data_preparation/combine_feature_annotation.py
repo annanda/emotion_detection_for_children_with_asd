62b0806ae9a9b64c5bce82c43d01e591e3db1cec
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-01 09:26:15 +0100

    refactoring.

--- data_preparation/combine_feature_annotation.py
+++ data_preparation/combine_feature_annotation.py
@@ -7,8 +7,7 @@ import os.path
 import pandas as pd
 from scipy.io import arff
 import glob
-
-path_main_folder = pathlib.Path(__file__).parent.parent.absolute()
+from setup.conf import MAIN_FOLDER
 
 
 def merge_features_with_annotation(file_features, list_file_name_emotions):
@@ -20,7 +19,7 @@ def merge_features_with_annotation(file_features, list_file_name_emotions):
     data_emotion_annotation_df = pd.read_csv(file_emotions)
     merged = pd.merge(data_features_df, data_emotion_annotation_df, how='inner', on='frametime')
     merged.dropna(inplace=True)
-    path_dataset = os.path.join(path_main_folder, 'dataset', 'video', 'AU')
+    path_dataset = os.path.join(MAIN_FOLDER, 'dataset', 'video', 'AU')
     merged.to_csv(f"{path_dataset}/{file_name_suffix}.csv", index=False)
 
 
@@ -33,7 +32,7 @@ def call_merge_video_au(list_file_annotation):
     """
     calling the function merge_for_all_files() for audio modality - features Action Units (AU)
     """
-    path_features_train = os.path.join(path_main_folder, 'features', 'video', 'AU')
+    path_features_train = os.path.join(MAIN_FOLDER, 'features', 'video', 'AU')
     list_file_features_train = glob.glob(f"{path_features_train}/*.arff")
     # eliminating the test files from the list, because we don't have annotation for test files.
     list_file_features_train = [file for file in list_file_features_train if 'test' not in file]
@@ -42,6 +41,6 @@ def call_merge_video_au(list_file_annotation):
 
 
 if __name__ == '__main__':
-    path_annotation_emotions = os.path.join(path_main_folder, 'labels', 'emotion_zones', 'emotion_names')
+    path_annotation_emotions = os.path.join(MAIN_FOLDER, 'labels', 'emotion_zones', 'emotion_names')
     list_file_annotation_emotions = glob.glob(f"{path_annotation_emotions}/*.csv")
     call_merge_video_au(list_file_annotation_emotions)
