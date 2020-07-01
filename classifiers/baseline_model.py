03595b116ef3d5bdfdf6cbf84114cbae4b583ac7
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-01 11:46:48 +0100

    Creating a dummy model as baseline.

--- classifiers/baseline_model.py
+++ classifiers/baseline_model.py
@@ -1,2 +1,11 @@
-import numpy as np
 from sklearn.dummy import DummyClassifier
+import pandas as pd
+from setup.conf import DATASET_VIDEO_FOLDER
+
+x = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv').iloc[:, 2:-1]
+y = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv')['emotion_zone']
+# dummy_clf = DummyClassifier(strategy="most_frequent")
+dummy_clf = DummyClassifier(strategy="uniform")
+dummy_clf.fit(x, y)
+predictions = dummy_clf.predict(x[:10])
+print(predictions)
--- setup/conf.py
+++ setup/conf.py
@@ -10,3 +10,4 @@ emotion_folder = os.path.join(MAIN_FOLDER, 'labels', 'emotion_zones', 'emotion_n
 
 EMOTION_ANNOTATION_FOLDER = config('EMOTION_ANNOTATION_FOLDER', default=emotion_folder)
 DATASET_VIDEO_AU_FOLDER = config('DATASET_VIDEO_AU', default=os.path.join(MAIN_FOLDER, 'dataset/video/AU'))
+DATASET_VIDEO_FOLDER = config('DATASET_VIDEO_FOLDER', default=os.path.join(MAIN_FOLDER, 'dataset/video'))
