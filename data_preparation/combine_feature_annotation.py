fc6f99756a619350678a07aa7246ba0ed6b80527
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-03 08:17:43 +0100

    making the script more generic to include other features type.

--- data_preparation/combine_feature_annotation.py
+++ data_preparation/combine_feature_annotation.py
@@ -10,7 +10,7 @@ import glob
 from setup.conf import MAIN_FOLDER
 
 
-def merge_features_with_annotation(file_features, list_file_name_emotions):
+def merge_features_with_annotation(file_features, list_file_name_emotions, feature_type):
     file_name_suffix = file_features.split('/')[-1]
     file_name_suffix = file_name_suffix.split('_3.0_0')[0]
     data_features = arff.loadarff(file_features)
@@ -19,28 +19,31 @@ def merge_features_with_annotation(file_features, list_file_name_emotions):
     data_emotion_annotation_df = pd.read_csv(file_emotions)
     merged = pd.merge(data_features_df, data_emotion_annotation_df, how='inner', on='frametime')
     merged.dropna(inplace=True)
-    path_dataset = os.path.join(MAIN_FOLDER, 'dataset', 'video', 'AU')
+    path_dataset = os.path.join(MAIN_FOLDER, 'dataset', 'video', f'{feature_type}')
     merged.to_csv(f"{path_dataset}/{file_name_suffix}.csv", index=False)
 
 
-def merge_for_all_files(list_file_features, list_file_annotation):
+def merge_for_all_files(list_file_features, list_file_annotation, feature_type):
     for list_train in list_file_features:
-        merge_features_with_annotation(list_train, list_file_annotation)
+        merge_features_with_annotation(list_train, list_file_annotation, feature_type)
 
 
-def call_merge_video_au(list_file_annotation):
+def call_merge_video_au(list_file_annotation, feature_type):
     """
     calling the function merge_for_all_files() for audio modality - features Action Units (AU)
     """
-    path_features_train = os.path.join(MAIN_FOLDER, 'features', 'video', 'AU')
+    path_features_train = os.path.join(MAIN_FOLDER, 'features', 'video', f'{feature_type}')
     list_file_features_train = glob.glob(f"{path_features_train}/*.arff")
     # eliminating the test files from the list, because we don't have annotation for test files.
     list_file_features_train = [file for file in list_file_features_train if 'test' not in file]
     list_file_features_train.sort()
-    merge_for_all_files(list_file_features_train, list_file_annotation)
+    merge_for_all_files(list_file_features_train, list_file_annotation, feature_type)
 
 
 if __name__ == '__main__':
     path_annotation_emotions = os.path.join(MAIN_FOLDER, 'labels', 'emotion_zones', 'emotion_names')
     list_file_annotation_emotions = glob.glob(f"{path_annotation_emotions}/*.csv")
-    call_merge_video_au(list_file_annotation_emotions)
+    # feature_type is part of the set {'AU', 'appearance'}
+    # feature_type = 'AU'
+    feature_type = 'appearance'
+    call_merge_video_au(list_file_annotation_emotions, feature_type)
