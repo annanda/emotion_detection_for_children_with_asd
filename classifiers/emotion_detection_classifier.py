5b8816ed0074e1dead574d1abc8293d05d651679
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-03 07:31:51 +0100

    organising the code.

--- classifiers/emotion_detection_classifier.py
+++ classifiers/emotion_detection_classifier.py
@@ -5,17 +5,23 @@ from sklearn import svm
 
 from sklearn.model_selection import train_test_split
 
-x = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv').iloc[:, 2:-1]
-y = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_train.csv')['emotion_zone']
 
-x_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_dev.csv').iloc[:, 2:-1]
-y_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/AU_dev.csv')['emotion_zone']
-x_dev, x_test = train_test_split(x_dev_dataset, test_size=0.2)
-y_dev, y_test = train_test_split(y_dev_dataset, test_size=0.2)
+def run_model(video_feature, model):
+    x = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/{video_feature}_train.csv').iloc[:, 2:-1]
+    y = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/{video_feature}_train.csv')['emotion_zone']
+    x_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/{video_feature}_dev.csv').iloc[:, 2:-1]
+    y_dev_dataset = pd.read_csv(f'{DATASET_VIDEO_FOLDER}/{video_feature}_dev.csv')['emotion_zone']
+    x_dev, x_test = train_test_split(x_dev_dataset, test_size=0.2)
+    y_dev, y_test = train_test_split(y_dev_dataset, test_size=0.2)
 
-clf = svm.SVC()
-clf.fit(x, y)
+    if model == 'SVM':
+        clf = svm.SVC()
+    clf.fit(x, y)
 
-predictions = clf.predict(x_test)
-accuracy = accuracy_score(y_test, predictions)
-print(f'Accuracy of SVM model: {accuracy}')
+    predictions = clf.predict(x_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f'Accuracy of SVM model: {accuracy}')
+
+
+if __name__ == '__main__':
+    run_model('AU', 'SVM')
