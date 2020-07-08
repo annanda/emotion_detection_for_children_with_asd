40b4efe60ba367d0b8c08090fa98b5fa3f23bcdd
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-08 12:51:36 +0100

    Creating a function to deal with modalities.

--- main.py
+++ main.py
@@ -7,19 +7,50 @@ from classifiers.emotion_detection_classifier import run_model_more_than_one_fea
 from data_preparation.combine_feature_annotation import call_merge_video_files
 from data_preparation.concatenating_datasets import concatenate_dataset_files
 from setup.conf import DATASET_FOLDER
+from classifiers.late_fusion_layer import late_fusion
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 
 def run_system(modality, features_type, model):
     if modality not in ['video', 'audio']:
         raise TypeError('Modality must be video or audio')
     if len(features_type) == 1:
-        accuracy, confusion_mtrx = run_model_one_feature_type(modality, features_type[0], model)
+        # accuracy, confusion_mtrx = run_model_one_feature_type(modality, features_type[0], model)
+        predictions, y_test = run_model_one_feature_type(modality, features_type[0], model)
     else:
-        accuracy, confusion_mtrx = run_model_more_than_one_feature_type(modality, features_type, model)
+        # accuracy, confusion_mtrx = run_model_more_than_one_feature_type(modality, features_type, model)
+        predictions, y_test = run_model_more_than_one_feature_type(modality, features_type, model)
 
-    print(f'Accuracy of {model} model for {modality} modality using {features_type} as features: {accuracy}')
+    #  to not break the code yet.
+    #####################################
+    accuracy_1 = accuracy_score(y_test, predictions)
+
+    confusion_mtrx_1 = confusion_matrix(y_test, predictions, labels=["blue", "green", "yellow", "red"])
+
+    print(f'Accuracy of {model} model for {modality} modality using {features_type} as features: {accuracy_1}')
     print('Confusion matrix: labels=["blue", "green", "yellow", "red"]')
-    print(confusion_mtrx)
+    print(confusion_mtrx_1)
+    #####################################
+
+    return predictions, y_test
+
+
+def computing_modalities(modality, features_type, model):
+    predictions_1, y_test_1 = run_system(modality, features_type, model)
+    predictions_2, y_test_2 = run_system(modality, features_type, model)
+
+    predictions = late_fusion(predictions_1, predictions_2)
+
+    accuracy_1 = accuracy_score(y_test_1, predictions)
+    accuracy_2 = accuracy_score(y_test_2, predictions)
+
+    confusion_mtrx_1 = confusion_matrix(y_test_1, predictions, labels=["blue", "green", "yellow", "red"])
+    confusion_mtrx_2 = confusion_matrix(y_test_2, predictions, labels=["blue", "green", "yellow", "red"])
+
+    print(f'Accuracy of {model} model for {modality} modality using {features_type} as features: {accuracy_1}')
+    print('Confusion matrix: labels=["blue", "green", "yellow", "red"]')
+    print(confusion_mtrx_1)
 
 
 def prepare_data(modality, features_type):
@@ -34,7 +65,6 @@ def system_entry(modality, features_type, model):
     if modality not in ['video', 'audio']:
         raise TypeError('Modality must be video or audio')
     if len(features_type) == 1:
-        # features_type = features_type[0]
         path_to_check = f'{DATASET_FOLDER}/{modality}/{features_type[0]}_dev.csv'
         if os.path.isfile(path_to_check):
             run_system(modality, features_type, model)
@@ -57,24 +87,11 @@ def prepare_systems_input(input_data):
 
 
 if __name__ == '__main__':
-    # configurations I want to use when running the system
-    # features_type = ['BoAW']
-    # features_type = ['AU', 'appearance', 'BoVW', 'geometric']
-    # features_type = ['AU', 'geometric']
-    # model = 'SVM'
-    # modality = 'video'
-
-    # run first prepare_data() if you are running one feature type only and for the first time
-    # prepare_data(modality, features_type)
-
-    # then run run_system()
-    # run_system(modality, features_type, model)
-
     input_data = {
         'video': True,
         'audio': None,
         'fusion_type': None,
-        'features_type_video': {'AU': True, 'appearance': True, 'BoVW': None, 'geometric': None},
+        'features_type_video': {'AU': True, 'appearance': False, 'BoVW': None, 'geometric': False},
         'features_type_audio': {'BoAW': None},
         'model_audio': None,
         'model_video': 'SVM'
