58d5b5db3d71be040bf60d69427b40c51dc17d1a
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-10 12:09:39 +0100

    Fixing the error when splitting dataset into dev/test

--- classifiers/emotion_detection_classifier.py
+++ classifiers/emotion_detection_classifier.py
@@ -9,18 +9,24 @@ from data_preparation.concatenating_datasets import producing_more_than_one_feat
 
 
 def run_model_one_feature_type(modality, feature_type, model):
-    x = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_train.csv').iloc[:, 2:-1]
+    x = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_train.csv').iloc[:, 1:-1]
     y = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_train.csv')['emotion_zone']
-    x_dev_dataset = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_dev.csv').iloc[:, 2:-1]
-    y_dev_dataset = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_dev.csv')['emotion_zone']
-    x_dev, x_test = train_test_split(x_dev_dataset, test_size=0.2)
-    y_dev, y_test = train_test_split(y_dev_dataset, test_size=0.2)
+    x_dev_dataset = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_dev.csv').iloc[:, 1:-1]
+    y_dev_dataset = pd.read_csv(f'{DATASET_FOLDER}/{modality}/{feature_type}_dev.csv')[['frametime', 'emotion_zone']]
+
+    x_dev, x_test, y_dev, y_test = train_test_split(x_dev_dataset, y_dev_dataset, test_size=0.2)
 
     if model == 'SVM':
         clf = svm.SVC()
     clf.fit(x, y)
 
     predictions = clf.predict(x_test)
+    predictions_df = pd.DataFrame(predictions, columns=['prediction'])
+    # predictions_df_merged = pd.merge(predictions_df, y_test, left_on='prediction', right_on='emotion_zone')
+    y_test['predictions'] = predictions
+
+    # TODO fix the return of this function - make the next one work with the y_test df intead of two different
+    # variables.
     return predictions, y_test
 
 
