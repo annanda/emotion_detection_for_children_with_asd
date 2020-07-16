af72dd439388cb756859d823b982a4434b634b0c
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-16 09:38:40 +0100

    For now, systems only working with one or two different modalities.

--- classifiers/late_fusion_layer.py
+++ classifiers/late_fusion_layer.py
@@ -7,6 +7,26 @@
 import random
 
 
+# def late_fusion(list_of_predictions):
+#     # prediction_2 = ['red' for _ in range(len(prediction_1))]
+#     if len(prediction_1) != len(prediction_2):
+#         raise TypeError('The two prediction vectors must be the same length')
+#     result_prediction = []
+#     for i in range(len(prediction_1)):
+#         if not prediction_1[i] or prediction_2[i]:
+#             if prediction_1[i]:
+#                 result_prediction.append(prediction_1[i])
+#             else:
+#                 result_prediction.append(prediction_2[i])
+#             continue
+#         pick = random.choice([1, 2])
+#         if pick == 1:
+#             result_prediction.append(prediction_1[i])
+#         else:
+#             result_prediction.append(prediction_2[i])
+#
+#     return result_prediction
+
 def late_fusion(prediction_1, prediction_2):
     # prediction_2 = ['red' for _ in range(len(prediction_1))]
     if len(prediction_1) != len(prediction_2):
