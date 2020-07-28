e0ca342f8602d079b2cc1197cbaf6ed983617fe7
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-07-28 13:04:44 +0100

    Creating a function to run the system several times and compute the average accuracy obtained.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from classifiers.emotion_detection_classifier import get_features_and_model, cal
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 def run_system(data_entry):
@@ -28,18 +29,38 @@ def run_system(data_entry):
         predictions = get_final_label_prediction_array(predictions_and_y_test)
 
     y_test = predictions_and_y_test['emotion_zone'].tolist()
-    calculate_evaluation_metrics(predictions, y_test, data_entry)
+    accuracy, confusion_mtrx = calculate_evaluation_metrics(predictions, y_test)
+    return accuracy, confusion_mtrx
 
 
-def calculate_evaluation_metrics(predictions, y_test, data_entry):
+def calculate_evaluation_metrics(predictions, y_test):
     accuracy = accuracy_score(y_test, predictions)
     confusion_mtrx = confusion_matrix(y_test, predictions, labels=["blue", "green", "yellow", "red"])
+    return accuracy, confusion_mtrx
 
+
+def print_results(accuracy, confusion_mtrx, data_entry, is_mean=False, times=None):
     print(f'Processing the input: {data_entry}')
     print('###########################################')
-    print(f'Accuracy: {accuracy}')
-    print('Confusion matrix: labels=["blue", "green", "yellow", "red"]')
+    if is_mean:
+        print(f'Average of accuracy by running system {times} times: {accuracy}')
+        print('Last computed Confusion matrix: labels=["blue", "green", "yellow", "red"]')
+    else:
+        print(f'Accuracy: {accuracy}')
+        print('Confusion matrix: labels=["blue", "green", "yellow", "red"]')
     print(confusion_mtrx)
+
+
+def run_x_times(times, data_entry):
+    accuracy_values = []
+    for _ in range(times):
+        accuracy, confusion_mtrx = run_system(data_entry)
+        accuracy_values.append(accuracy)
+
+    accuracy_values = np.array(accuracy_values)
+    print(accuracy_values)
+    mean_accuracy = np.mean(accuracy_values)
+    print_results(mean_accuracy, confusion_mtrx, data_entry, is_mean=True, times=times)
 
 
 if __name__ == '__main__':
@@ -49,15 +70,17 @@ if __name__ == '__main__':
                 'features_type': {'AU': True, 'appearance': False, 'BoVW': False, 'geometric': False},
                 'model': 'SVM'
             },
-            'audio': {
-                'features_type': {'BoAW': True, 'DeepSpectrum': False, 'eGeMAPSfunct': False},
-                # eGeMAPSfunct feature_type can only be used alone
-                'model': 'SVM'
-            },
-            'physio': {
-                'features_type': {'HRHRV': True},
-                'model': 'SVM'
-            }
+            # 'audio': {
+            #     'features_type': {'BoAW': True, 'DeepSpectrum': False, 'eGeMAPSfunct': False},
+            #     # eGeMAPSfunct feature_type can only be used alone
+            #     'model': 'SVM'
+            # },
+            # 'physio': {
+            #     'features_type': {'HRHRV': True},
+            #     'model': 'SVM'
+            # }
         },
         'fusion_type': 'late_fusion'}
-    run_system(input_data)
+    # accuracy, confusion_mtrx = run_system(input_data)
+    # print_results(accuracy, confusion_mtrx, input_data)
+    run_x_times(5, input_data)
