e9f93683d2aaf40a3a9ead62cc861f1543bd4350
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-11-22 08:39:28 +0000

    Data experiments json configuration and txt results.

--- emotion_detection_system/scripts/data_experiments_batch_01.sh
+++ emotion_detection_system/scripts/data_experiments_batch_01.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 # All features included
-python entry_point.py participant_02_va_late_fusion.json> results/participant_02_va_late_fusion.txt
-#python entry_point.py participant_02_v_SVM.json > results/participant_02_v_SVM.txt
-#python entry_point.py participant_02_a_SVM.json > results/participant_02_a_SVM.txt
+# All participants separately - Video&Audio: all features - Late fusion
 
+#echo "Starting new Data Experiment Configuration"
+#python entry_point.py participant_01_va_late_fusion.json> results/participant_01_va_late_fusion.txt
+#echo "Starting new Data Experiment Configuration"
+# python entry_point.py participant_02_va_late_fusion.json> results/participant_02_va_late_fusion.txt
+#echo "Starting new Data Experiment Configuration"
+#python entry_point.py participant_03_va_late_fusion.json> results/participant_03_va_late_fusion.txt
+#echo "Starting new Data Experiment Configuration"
+#python entry_point.py participant_04_va_late_fusion.json> results/participant_04_va_late_fusion.txt
+
+
+# All sessions separately - Video&Audio: all features - Late fusion
+#echo "Starting new Data Experiment Configuration: session_01_01_va_late_fusion"
+#python entry_point.py session_01_01_va_late_fusion.json> results/session_01_01_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_02_01_va_late_fusion"
+#python entry_point.py session_02_01_va_late_fusion.json> results/session_02_01_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_02_02_va_late_fusion"
+#python entry_point.py session_02_02_va_late_fusion.json> results/session_02_02_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_03_01_va_late_fusion"
+#python entry_point.py session_03_01_va_late_fusion.json> results/session_03_01_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_03_02_va_late_fusion"
+#python entry_point.py session_03_02_va_late_fusion.json> results/session_03_02_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_04_01_va_late_fusion"
+#python entry_point.py session_04_01_va_late_fusion.json> results/session_04_01_va_late_fusion.txt
+#
+#echo "Starting new Data Experiment Configuration: session_04_02_va_late_fusion"
+#python entry_point.py session_04_02_va_late_fusion.json> results/session_04_02_va_late_fusion.txt
+
+# All participants together - Video&Audio: all features - Late fusion
+echo "Starting new Data Experiment Configuration: all_data_va_late_fusion"
+python entry_point.py all_data_va_late_fusion.json> results/all_data_va_late_fusion.txt
+
+# All participants together - Video&Audio: all features - Early fusion
+echo "Starting new Data Experiment Configuration: all_data_va_early_fusion"
+python entry_point.py all_data_va_early_fusion.json> results/all_data_va_early_fusion.txt
