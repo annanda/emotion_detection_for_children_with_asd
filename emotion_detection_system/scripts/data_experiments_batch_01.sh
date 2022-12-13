f0fb92cb3515d7380ab6e90e3399374b0c4623ff
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-12-13 15:28:15 +0000

    Adding Json for more experiments - unimodals and updating the script to include them.

--- emotion_detection_system/scripts/data_experiments_batch_01.sh
+++ emotion_detection_system/scripts/data_experiments_batch_01.sh
@@ -8,13 +8,13 @@
 #MULTIMODAL
 # Late Fusion
 # All participants separately - Video&Audio: features (above) - Late fusion
-echo "Starting new Data Experiment Configuration"
+echo "Starting new Data Experiment Configuration: participant_01_va_late_fusion.json"
 python entry_point.py participant_01_va_late_fusion.json> results/participant_01_va_late_fusion.txt
-echo "Starting new Data Experiment Configuration"
+echo "Starting new Data Experiment Configuration: participant_02_va_late_fusion.json"
  python entry_point.py participant_02_va_late_fusion.json> results/participant_02_va_late_fusion.txt
-echo "Starting new Data Experiment Configuration"
+echo "Starting new Data Experiment Configuration: participant_03_va_late_fusion"
 python entry_point.py participant_03_va_late_fusion.json> results/participant_03_va_late_fusion.txt
-echo "Starting new Data Experiment Configuration"
+echo "Starting new Data Experiment Configuration: participant_04_va_late_fusion"
 python entry_point.py participant_04_va_late_fusion.json> results/participant_04_va_late_fusion.txt
 
 
@@ -90,6 +90,7 @@ python entry_point.py all_data_va_early_fusion.json> results/all_data_va_early_f
 
 # UNIMODAL
 # Unimodality
+# VIDEO
 # Each participants separately - Video:  features (above) - SVM
 echo "Starting new Data Experiment Configuration: participant_01_v.json"
 python entry_point.py participant_01_v.json> results/participant_01_v.txt
@@ -104,3 +105,59 @@ echo "Starting new Data Experiment Configuration: participant_04_v.json"
 python entry_point.py participant_04_v.json> results/participant_04_v.txt
 
 # Each session separately - Video:  features (above) - SVM
+echo "Starting new Data Experiment Configuration: session_01_01_v.json"
+python entry_point.py session_01_01_v.json> results/session_01_01_v.txt
+
+echo "Starting new Data Experiment Configuration: session_02_01_v.json"
+python entry_point.py session_02_01_v.json> results/session_02_01_v.txt
+
+echo "Starting new Data Experiment Configuration: session_02_02_v.json"
+python entry_point.py session_02_02_v.json> results/session_02_02_v.txt
+
+echo "Starting new Data Experiment Configuration: session_03_01_v.json"
+python entry_point.py session_03_01_v.json> results/session_03_01_v.txt
+
+echo "Starting new Data Experiment Configuration: session_03_02_v.json"
+python entry_point.py session_03_02_v.json> results/session_03_02_v.txt
+
+echo "Starting new Data Experiment Configuration: session_04_01_v.json"
+python entry_point.py session_04_01_v.json> results/session_04_01_v.txt
+
+echo "Starting new Data Experiment Configuration: session_04_02_v.json"
+python entry_point.py session_04_02_v.json> results/session_04_02_v.txt
+
+# AUDIO
+# Each participants separately - Audio:  all - SVM
+echo "Starting new Data Experiment Configuration: participant_01_a.json"
+python entry_point.py participant_01_a.json> results/participant_01_a.txt
+
+echo "Starting new Data Experiment Configuration: participant_02_a.json"
+python entry_point.py participant_02_a.json> results/participant_02_a.txt
+
+echo "Starting new Data Experiment Configuration: participant_03_a.json"
+python entry_point.py participant_03_a.json> results/participant_03_a.txt
+
+echo "Starting new Data Experiment Configuration: participant_04_a.json"
+python entry_point.py participant_04_a.json> results/participant_04_a.txt
+
+# Each session separately - Audio:  features all - SVM
+echo "Starting new Data Experiment Configuration: session_01_01_a.json"
+python entry_point.py session_01_01_a.json> results/session_01_01_a.txt
+
+echo "Starting new Data Experiment Configuration: session_02_01_a.json"
+python entry_point.py session_02_01_a.json> results/session_02_01_a.txt
+
+echo "Starting new Data Experiment Configuration: session_02_02_a.json"
+python entry_point.py session_02_02_a.json> results/session_02_02_a.txt
+
+echo "Starting new Data Experiment Configuration: session_03_01_a.json"
+python entry_point.py session_03_01_a.json> results/session_03_01_a.txt
+
+echo "Starting new Data Experiment Configuration: session_03_02_a.json"
+python entry_point.py session_03_02_a.json> results/session_03_02_a.txt
+
+echo "Starting new Data Experiment Configuration: session_04_01_a.json"
+python entry_point.py session_04_01_a.json> results/session_04_01_a.txt
+
+echo "Starting new Data Experiment Configuration: session_04_02_a.json"
+python entry_point.py session_04_02_a.json> results/session_04_02_a.txt
